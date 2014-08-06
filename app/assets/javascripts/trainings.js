ec5da14335eecac6d76f50325572b182e1c659ab
Juande Ruiz <juanderuiz@gmail.com>
2014-08-06 19:18:48 +0200

    Change some js and css files

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-	//Muestra los comentarios
-	$('.training').on('click','.enlacecomentarios',function(event){
+	//Muestra los comentarios en el OnOver
+	$('.training').on('mouseover','.enlacecomentarios',function(event){
 	   event.preventDefault();//para que no haga scroll la página
        $(this).hide();
-       $(this).closest('.training').find('.comments').fadeToggle();
+       $(this).closest('.comentarios').find('.comments').css({'color':'yellow'}).fadeToggle();
+    });
+    $('.training').on('mouseleave','.comments',function(event){
+       event.preventDefault();//para que no haga scroll la página
+       $(this).hide();
+       $(this).closest('.comentarios').find('.enlacecomentarios').fadeToggle();
     });
     //Tiempo resaltado
     $('.training').on('mouseover','.time',function(){
-    	$(this).css({'color':'#ffd','font-size':'14px'});
+    	$(this).css({'color':'#ffd','font-size':'17px'});
     });
     $('.training').on('mouseleave','.time',function(){
         $(this).css({'font-size':'16px'});
