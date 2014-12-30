482651978097ecabafbda2402284a52056f2eccb
Juande Ruiz <juanderuiz@gmail.com>
2014-12-30 17:44:10 +0100

    Fix problem CASTing Integer Type in Trainings table.

--- db/migrate/20140123163835_change_time_type_to_integer.rb
+++ db/migrate/20140123163835_change_time_type_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeTimeTypeToInteger < ActiveRecord::Migration
   def up
-    change_column :trainings, :time, :integer
+    change_column :trainings, :time, 'integer USING CAST(time AS integer)'
   end
 
   def down
